993514f8456582e06497eeaeb1fdba7accbfddbb
YenNhi <nguyenyennhi117@gmail.com>
2022-04-04 15:25:03 +0700

    Styling elements

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
   function handleSubmit(event) {
     event.preventDefault();
     setSearch(true);
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7e89fd679dec71112f47fb6b0d6af5c6&units=metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d800d7a77d154e7030aa4c43d236a01f&units=metric`;
     axios.get(url).then(changeData);
   }
 
@@ -31,61 +31,71 @@ function App() {
   }
 
   function defaultCity(city1) {
-    let urlDefault = `https://api.openweathermap.org/data/2.5/weather?q=${city1}&appid=7e89fd679dec71112f47fb6b0d6af5c6&units=metric`;
+    let urlDefault = `https://api.openweathermap.org/data/2.5/weather?q=${city1}&appid=d800d7a77d154e7030aa4c43d236a01f&units=metric`;
     axios.get(urlDefault).then(changeData);
   }
 
   let displayInfo = (
-    <div className="App">
-      <form className="mb-3" onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Change your location"
-              className="form-control"
-              autoComplete="off"
-              onChange={changeCity}
-            />
+    <div>
+      <div className="App">
+        <form className="mb-3" onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Change your location"
+                className="form-control"
+                autoComplete="off"
+                onChange={changeCity}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
+        </form>
+        <div className="current-city">
+          <h1>{weatherData.city}</h1>
+          <ul>
+            <li>Last updated: {weatherData.date}</li>
+            <li className="text-capitalize">{weatherData.description}</li>
+          </ul>
         </div>
-      </form>
-      <div className="current-city">
-        <h1>{weatherData.city}</h1>
-        <ul>
-          <li>Last updated: {weatherData.date}</li>
-          <li>{weatherData.description}</li>
-        </ul>
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="weather-temperature">
-            <img
-              src={weatherData.icon}
-              alt={weatherData.description}
-              className="float-left"
-            />
-            <div>
-              <strong className="Temperature">{weatherData.temperature}</strong>
-              <span className="units">
-                <a href="/">°C</a> | <a href="/">°F</a>
+        <div className="row">
+          <div className="col-7">
+            <div className="d-flex align-end">
+              <img
+                src={weatherData.icon}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span>
+                <strong className="Temperature">
+                  {weatherData.temperature}
+                </strong>
+                <span className="units">
+                  <a href="/">°C</a> | <a href="/">°F</a>
+                </span>
               </span>
             </div>
           </div>
+          <div className="col-5">
+            <ul>
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
+          </div>
         </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {weatherData.humidity}%</li>
-            <li>Wind: {weatherData.wind} km/h</li>
-          </ul>
-        </div>
+      </div>
+      <div className="footer">
+        <a href="https://github.com/YenNhi96/weatherapp-react">
+          Open-source code
+        </a>{" "}
+        by Nhi Nguyen
       </div>
     </div>
   );
