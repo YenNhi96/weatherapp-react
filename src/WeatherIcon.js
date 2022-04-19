80d728c2692509205de3fc69fed15716a7484d05
YenNhi <nguyenyennhi117@gmail.com>
2022-04-19 16:12:24 +0700

    create daily forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,27 +1,34 @@
 import React from "react";
-import { OpenWeatherMapCodes } from "react-animated-climacons";
-import "./Weather.css";
+import ReactAnimatedWeather from "react-animated-weather";
 
 export default function WeatherIcon(props) {
-  const Night = OpenWeatherMapCodes.night[props.data];
-  const Day = OpenWeatherMapCodes.day[props.data];
+  const codeMapping = {
+    "01d": "CLEAR_DAY",
+    "01n": "CLEAR_NIGHT",
+    "02d": "PARTLY_CLOUDY_DAY",
+    "02n": "PARTLY_CLOUDY_NIGHT",
+    "03d": "PARTLY_CLOUDY_DAY",
+    "03n": "PARTLY_CLOUDY_NIGHT",
+    "04d": "CLOUDY",
+    "04n": "CLOUDY",
+    "09d": "RAIN",
+    "09n": "RAIN",
+    "10d": "RAIN",
+    "10n": "RAIN",
+    "11d": "RAIN",
+    "11n": "RAIN",
+    "13d": "SNOW",
+    "13n": "SNOW",
+    "50d": "FOG",
+    "50n": "FOG",
+  };
 
-  let dateData = new Date(props.dateInfo);
-  let hours = dateData.getHours();
-
-  if (hours <= 16) {
-    return (
-      <div className="weather-icon">
-        <Day fill="black" stroke="grey" />
-      </div>
-    );
-  }
-
-  if (hours > 16) {
-    return (
-      <div className="weather-icon">
-        <Night fill="black" stroke="grey" />
-      </div>
-    );
-  }
+  return (
+    <ReactAnimatedWeather
+      icon={codeMapping[props.code]}
+      color="#1e1e1e"
+      size={props.size}
+      animate={true}
+    />
+  );
 }
